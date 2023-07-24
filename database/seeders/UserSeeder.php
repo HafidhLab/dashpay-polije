bd9d739797a676c4787bce0712357ba1f021720b
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-07-24 16:41:06 +0700

    add: user seeder with permission

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $superuser = User::create([
+            'name' => 'Super User',
+            'email' => 'superuser@mail.com',
+            'password' => Hash::make('superuser123'),
+        ]);
+
+        $admin = User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('admin123'),
+        ]);
+
+        $auditor = User::create([
+            'name' => 'Auditor',
+            'email' => 'auditor@mail.com',
+            'password' => Hash::make('auditor123'),
+        ]);
+
+        $superuser->assignRole('superuser');
+        $admin->assignRole('admin');
+        $auditor->assignRole('auditor');
+    }
+}
