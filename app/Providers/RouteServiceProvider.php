47dda63cfe2447c8de726acaaaf9c7b15b2cddf0
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-07-24 16:35:41 +0700

    add: roles and it routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,6 +33,20 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
+            Route::middleware('web')->group(function() {
+                Route::prefix('superuser')
+                    ->name('superuser.')
+                    ->group(base_path('routes/web/superuser.php'));
+
+                Route::prefix('admin')
+                    ->name('admin.')
+                    ->group(base_path('routes/web/admin.php'));
+
+                Route::prefix('auditor')
+                    ->name('auditor.')
+                    ->group(base_path('routes/web/auditor.php'));
+            });
+
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
