3f5404cbec3695f22a07c45284adb0637e9d6897
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-07-24 18:09:53 +0700

    chore: login with livewire method

--- app/Http/Livewire/Auth/Login.php
+++ app/Http/Livewire/Auth/Login.php
@@ -6,9 +6,21 @@ use Livewire\Component;
 
 class Login extends Component
 {
+    public $email, $password;
+
+    protected $rules = [
+        'email' => 'required|email',
+        'password' => 'required|min:8',
+    ];
+
     public function render()
     {
         return view('livewire.auth.login')
             ->layout('layouts.auth');
+    }
+
+    public function login()
+    {
+        $this->validate();
     }
 }
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,28 +1,26 @@
 <div>
     <section class="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center">
         <div class="container">
-            <div class="row justify-content-center form-bg-image" data-background-lg="../../assets/img/illustrations/signin.svg">
+            <div class="row justify-content-center form-bg-image" data-background-lg="/assets/img/illustrations/signin.svg">
                 <div class="col-12 d-flex align-items-center justify-content-center">
                     <div class="bg-white shadow border-0 rounded border-light p-4 p-lg-5 w-100 fmxw-500">
                         <div class="text-center text-md-center mb-4 mt-md-0">
                             <h1 class="mb-0 h3">Sign in to Dashpay Polije</h1>
                         </div>
 
-                         <form method="POST" action="{{ route('login') }}" class="mt-4">
+                         <form wire:submit.prevent="login" class="mt-4">
                             @csrf
                             <!-- End of Form -->
                             <div class="form-group">
                                 <!-- Form -->
                                 <div class="form-group mb-4">
                                     <label for="email">{{ __('Email') }}</label>
-                                    <x-auth.text-input id="email" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="example@gmail.com" />
-                                    <x-auth.input-error :messages="$errors->get('email')" class="mt-2" />
+                                    <x-auth.text-input icon="fas fa-envelope" model="email" type="email" />
                                 </div>
                                 <!-- Form -->
                                 <div class="form-group mb-4">
                                     <label for="password">{{ __('Password') }}</label>
-                                    <x-auth.text-input id="password" type="password" name="password" :value="old('password')" placeholder="****" required autofocus autocomplete="password" />
-                                    <x-auth.input-error :messages="$errors->get('password')" class="mt-2" />
+                                    <x-auth.text-input icon="fas fa-lock" model="password" type="password" />
                                 </div>
                                 <!-- End of Form -->
                                 <div class="d-flex justify-content-between align-items-top mb-4">
