ebfd42f38207eb6c7b2d7744babca9118009c24f
destroylord <masapin68@gmail.com>
2023-08-12 01:32:20 +0700

    chore: add routes detail transaction by merchant

--- routes/web/auditor.php
+++ routes/web/auditor.php
@@ -5,3 +5,4 @@ use App\Http\Livewire\Auditor\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::get('dashboard', [DasboardController::class, 'index'])->name('dashboard');
+Route::get('merchant-transaction/{name}', [DasboardController::class, 'show'])->name('show');
